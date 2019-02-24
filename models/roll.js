431a4970442f7e694bc5022963247f09d310cf91
mhartenbower <matt.hartenbower@gmail.com>
2019-02-24 10:25:28 -0600

    Move roll save logic to the roll model

--- models/roll.js
+++ models/roll.js
@@ -1,18 +1,22 @@
+gameStore = require('./datastore');
+
 /*
 The roll model represents one roll.
 It consists of a number, and the value (number of pins knocked down)
 */
-const Roll = function(rollNum, value) {
-  this.rollNum = rollNum;
+const Roll = function(value) {
   this.value = value;
 };
 
 // Save the roll state to in-memory store
-Roll.prototype.save = function(gameId, player) {
+Roll.prototype.save = function(gameId, playerId, roll) {
   game = gameStore.getGame(gameId);
-  game.players[player.id] = player;
-  scoreboard = new Scoreboard();
-  game.scoreboard[player.id] = scoreboard;
+  err = game.addRoll(playerId, roll.value);
+  if (err) {
+    return err;
+  }
+  game.getScore(playerId);
+
   gameStore.setGame(game.id, game);
 };
 
--- routes/game.js
+++ routes/game.js
@@ -6,62 +6,81 @@ const Player = require('../models/player');
 const Roll = require(`../models/roll`);
 const {check, validationResult} = require('express-validator/check');
 
-
 const rollValidator = [
   check('rollValue').isInt({min: 0, max: 10}).withMessage('Roll must be between 0 and 10'),
 ];
 
-// Controller for creating a new game
+/*
+Controller for creating a new game
+Creates a new game and saves it.
+*/
 router.post('/', function(req, res, next) {
   gameUuid = uuidv4();
   game = new Game.Game(gameUuid);
   game.save(game);
+
   res.status(201).send({'msg': 'Game successfully created', 'game': game});
 });
 
-// Controller for retrieving a game
+/*
+Controller for retrieving a game
+*/
 router.get('/:gameId', function(req, res, next) {
   game = Game.Game.retrieve(req.params.gameId);
+
   res.status(200).json(game);
 });
 
-// Controller for creating a new player in game <gameId>
+/*
+ Controller for creating a new player in game <gameId>
+ Creates a new player and saves it.
+ */
 router.post('/:gameId/player', function(req, res, next) {
   playerUuid = uuidv4();
   playerName = req.body.name;
   player = new Player(playerUuid, req.body. playerName);
   player.save(req.params.gameId, player);
+
   res.status(201).send({'msg': 'Player successfully created', 'player': player});
 });
 
-// Controller for retrieving a player <playerId>
+/*
+Controller for retrieving a player <playerId>
+*/
 router.get('/:gameId/player/:playerId', function(req, res, next) {
   Player.retrieve(req.params.gameId, req.params.playerId);
 });
 
-// Controller for creating a new roll for player <playerId> in game <gameId>
+/*
+Controller for creating a new roll for player <playerId> in game <gameId>
+Creates a new roll and saves it.
+*/
 router.post('/:gameId/player/:playerId/roll', rollValidator, function(req, res, next) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).send({'msg': errors.array()[0].msg});
+  gameId = req.params.gameId;
+  playerId = req.params.playerId;
+  rollValue = parseInt(req.body.rollValue);
+
+  // Validate the body
+  let err = validationResult(req);
+  if (!err.isEmpty()) {
+    return res.status(400).send({'msg': err.array()[0].msg});
   }
-  game = Game.Game.retrieve(req.params.gameId);
-  err = game.addRoll(req.params.playerId, parseInt(req.body.rollValue));
+
+  roll = new Roll(rollValue);
+  err = roll.save(gameId, playerId, roll);
   if (err) {
     return res.status(400).send({'msg': err.toString()});
   }
-  for (playerId in game.players) {
-    if (game.players.hasOwnProperty(playerId)) {
-      game.getScore(playerId);
-    }
-  }
-  game.save(game);
-  res.status(201).send({'msg': 'Roll successful', 'game': game});
+
+  return res.status(201).send({'msg': 'Roll successful', 'game': game});
 });
 
-// Retrieve <rollNum> roll for player <playerId> in game <gameId>
+/*
+Retrieve <rollNum> roll for player <playerId> in game <gameId>
+*/
 router.get('/:gameId/player/:playerId/roll/:rollNum', function(req, res, next) {
   roll = Roll.retrieve(req.params.gameId, req.params.playerId, req.query.frameId, req.params.rollNum);
+
   res.status(200).send({'roll': roll});
 });
 
